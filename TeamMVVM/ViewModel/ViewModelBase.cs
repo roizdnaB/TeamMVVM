9bce3c5150140a603bac9ccbba123e1d783d5a26
roizdnaB <danieljambor1999@gmail.com>
2020-04-19 20:07:18 +0200

    Added ViewModelBase

---
+++ TeamMVVM/ViewModel/ViewModelBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace TeamMVVM.ViewModel
+{
+    class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void onPropertyChanged(params string[] namesOfProperties)
+        {
+            if(PropertyChanged != null)
+            {
+                foreach (var prop in namesOfProperties)
+                    PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
+        }
+    }
+}
