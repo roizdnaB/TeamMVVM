325eba618fa0ac3681bcd878d84ab3fd633cadff
roizdnaB <danieljambor1999@gmail.com>
2020-04-19 20:18:48 +0200

    Added player view model

--- TeamMVVM/Model/PlayerModel.cs
+++ TeamMVVM/Model/PlayerModel.cs
@@ -30,6 +30,8 @@ namespace TeamMVVM.Model
             this.weight = player.weight;
         }
 
+        public PlayerModel() { }
+
         public string FirstName { get { return firstName; } set { firstName = value; } }
         public string LastName { get { return lastName; } set { lastName = value; } }
         public ushort Age { get { return age; } set { age = value; } }
--- TeamMVVM/ViewModel/PlayerViewModel.cs
+++ TeamMVVM/ViewModel/PlayerViewModel.cs
@@ -4,9 +4,58 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace TeamMVVM.ViewModel
 {
-    class PlayerViewModel
+    using TeamMVVM.Model;
+ 
+    class PlayerViewModel : ViewModelBase
     {
+        private PlayerModel playerViewModel;
+        
+        public PlayerViewModel(PlayerModel player)
+        {
+            this.playerViewModel = player;
+        }
+        
+        public string FirstName
+        {
+            get { return playerViewModel.FirstName; }
+            set
+            {
+                playerViewModel.FirstName = value;
+                onPropertyChanged(nameof(FirstName));
+            }
+        }
+
+        public string LastName
+        {
+            get { return playerViewModel.LastName; }
+            set
+            {
+                playerViewModel.LastName = value;
+                onPropertyChanged(nameof(LastName));
+            }
+        }
+
+        public ushort Age
+        {
+            get { return playerViewModel.Age; }
+            set
+            {
+                playerViewModel.Age = value;
+                onPropertyChanged(nameof(LastName));
+            }
+        }
+
+        public double Weight
+        {
+            get { return playerViewModel.Weight; }
+            set
+            {
+                playerViewModel.Weight = value;
+                onPropertyChanged(nameof(Weight));
+            }
+        }
     }
 }
